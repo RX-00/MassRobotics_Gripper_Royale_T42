d2448659c006143bbb57cf2ad8e151430c1f1e63
RX-00 <00royxing@gmail.com>
2020-05-30 03:37:06 +0100

    modified project from my biped project and simplified it for this gripper

---
+++ build/main.d
@@ -0,0 +1,5 @@
+build/main.o: src/main.cpp include/RPMSerialInterface.h include/Utils.h
+
+include/RPMSerialInterface.h:
+
+include/Utils.h:
---
+++ src/main.cpp
@@ -0,0 +1,89 @@
+/* User demo program for the MassRobotics
+ * Royale T42 Gripper
+ * -Roy Xing, any questions please contact: roy@massrobotics.org
+ */
+
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <cmath>
+#include <string>
+#include <unistd.h>
+
+#include "RPMSerialInterface.h" // servos
+#include "Utils.h"              // servo utility class for sleep & time methods
+
+
+//NOTE: modified the RPM library to allow for greater servo range
+#define SRVO_MAX   10000
+#define SRVO_MIN   2000
+
+void servo_test(RPM::SerialInterface *servosInterface, unsigned char channelNumber){
+  std::cout << "Testing servo number: " << 11 << std::endl;
+  for (int i = 0; i < 5; i++){
+    std::cout << "min position" << std::endl;
+    servosInterface -> setTargetCP(channelNumber, SRVO_MIN);
+    Utils::sleep(1000);
+    std::cout << "middle position" << std::endl;
+    servosInterface -> setTargetCP(channelNumber, 6000);
+    Utils::sleep(1000);
+    std::cout << "max position" << std::endl;
+    servosInterface -> setTargetCP(channelNumber, SRVO_MAX);
+    Utils::sleep(1000);
+  }
+  exit(1);
+}
+
+// function to test device over serial w/ sinusoidal signals
+void sinusoid_signal(RPM::SerialInterface *serialInterface, unsigned char channelNumber){
+  // Generate a sinusoid signal to send to the PololuInterface
+  std::cout << "Sending sinusoidal signal to device to test device..." << std::endl;
+	const float pi = 3.141592653589793f;
+	const unsigned int channelMinValue = SRVO_MIN;
+	const unsigned int channelMaxValue = SRVO_MAX;
+	const unsigned int channelValueRange = channelMaxValue - channelMinValue;
+	const unsigned int signalPeriodInMs = 2000;
+	unsigned int time0 = Utils::getTimeAsMilliseconds();
+	unsigned int timeSinceStart = 0;
+	while ( timeSinceStart < 5000 ){
+    float k = sin( (pi*2)/signalPeriodInMs * timeSinceStart ) * (float)(channelValueRange/2);
+    float channelValue = (float)channelMinValue + (float)channelValueRange/2 + k;
+    printf("\rchannelValue=%d", (unsigned int)channelValue );
+    serialInterface->setTargetCP( channelNumber, (unsigned short)channelValue );
+    timeSinceStart = Utils::getTimeAsMilliseconds() - time0;
+    Utils::sleep(5);
+  }
+  printf("\n");
+}
+
+// function to create serial interface for the maestro servo controller
+RPM::SerialInterface * serialInterfaceInit(unsigned char deviceNumber, unsigned char channelNumber, std::string portName){
+  // create the interface for the maestro
+  std::cout << "Serial interface init..." << std::endl;
+	unsigned int baudRate = 115200;
+	printf("Creating serial interface '%s' at %d bauds\n", portName.c_str(), baudRate);
+	std::string errorMessage;
+	RPM::SerialInterface* serialInterface = RPM::SerialInterface::createSerialInterface( portName, baudRate, &errorMessage );
+	if ( !serialInterface ){
+    printf("Failed to create serial interface. %s\n", errorMessage.c_str());
+    std::cout << "Terminating program..." << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+  std::cout << "Serial interface initiated\n" << std::endl;
+  return serialInterface;
+}
+
+
+int main(int argc, char** argv){
+  // Serial servo interface
+  unsigned char deviceNumber = 12;
+  unsigned char channelNumber = 11;
+  std::string portName = "/dev/ttyACM1";
+  RPM::SerialInterface *servosInterface = serialInterfaceInit(deviceNumber, channelNumber, portName);
+  servosInterface -> SerialInterface::mMinChannelValue = SRVO_MIN;
+  servosInterface -> SerialInterface::mMaxChannelValue = SRVO_MAX;
+
+  delete servosInterface;
+  servosInterface = NULL;
+  return 0;
+}
